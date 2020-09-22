ce67e82b2b5db38e8bea4284b81f8f0be63d3e5a
gunslinger <yahyahassan7219@yahoo.com>
2020-09-22 21:06:59 +0300

    first

---
+++ pages/index.js
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import { Row, Col } from "antd";
+import { Card } from "antd";
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+
+
+const { Meta } = Card;
+const Home = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://picsum.photos/v2/list")
+      .then((resp) =>resp.json()).then((rez)=>setData(rez))}, [])
+      console.log(data)
+    return (
+    <div className="container">
+      <Row gutter={20} >
+      {data.map( (item, index)=> (
+      <Col xs={12} span={8}>
+        <a target="#" href={item.download_url}>
+        <Card
+        key={index}
+        hoverable
+        cover={
+        <LazyLoadImage
+          alt="example"
+          src={item.download_url} />}
+          >
+            <Meta title={item.author} description={item.url} />
+          </Card>
+          </a>
+      </Col>
+      ))}
+      </Row>
+    </div>
+  );
+};
+
+export default Home;
